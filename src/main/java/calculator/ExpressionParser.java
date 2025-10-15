5df73d0e093faebc89739929efde6c7d41c3789a
AlphaBs <ksi123456ab@naver.com>
2025-10-15 13:05:16 +0900

    feat(parser): extract numbers from input

--- src/main/java/calculator/ExpressionParser.java
+++ src/main/java/calculator/ExpressionParser.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import java.text.StringCharacterIterator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExpressionParser {
     public char parseCustomDelimiter(StringCharacterIterator iterator) {
@@ -21,5 +23,51 @@ public class ExpressionParser {
         }
         iterator.next();
         return delimiter;
+    }
+
+    public List<Long> parseNumbers(StringCharacterIterator iterator, List<Character> delimiters) {
+        List<Long> numbers = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+
+        // 빈 문자열 허용
+        if (iterator.current() == StringCharacterIterator.DONE) {
+            return numbers;
+        }
+
+        while (iterator.current() != StringCharacterIterator.DONE) {
+            if (Character.isDigit(iterator.current())) {
+                buffer.append(iterator.current());
+            }
+            else if (delimiters.contains(iterator.current())) {
+                numbers.add(parseNumber(buffer.toString()));
+                buffer.delete(0, buffer.length());
+            }
+            else {
+                throw new IllegalArgumentException("잘못된 구분자");
+            }
+
+            iterator.next();
+        }
+
+        // 버퍼가 비어있다면 구분자로 문자열이 끝난 경우
+        if (buffer.isEmpty()) {
+            throw new IllegalArgumentException("구분자로 끝난 문자열");
+        }
+
+        numbers.add(parseNumber(buffer.toString()));
+        return numbers;
+    }
+
+    private Long parseNumber(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("숫자가 올 자리에 빈 문자열이 있었습니다.");
+        }
+
+        try {
+            return Long.parseUnsignedLong(input);
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
